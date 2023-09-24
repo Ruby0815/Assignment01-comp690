e6cd3ccd4ebd0c8bd135283737ac1f404ed850cf
Ruby Verma <rubyverma@Rubys-MacBook-Air.local>
2023-09-23 18:32:00 -0700

    ADDED STEP 3 IN JS
    ADDED STEP3

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,10 @@ let mylibraryFunction;
 'hello';
 false;
 null;
+//STEP3
+//COMPLEX EXPRESSION
+let comExp1 =3+(98*3)+33;
+let comExp2 =55+(43%2)+1;
+//VARIABLE EXPRESSION
+let varEx1=33;
+let varExp2=11.33;
