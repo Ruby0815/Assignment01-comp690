80188a61dd31b5aab45d68b7d1236f02cf16e75d
Ruby Verma <rubyverma@Rubys-MacBook-Air.local>
2023-09-23 18:55:32 -0700

    added step5 in js
    added step 5

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,21 @@ function theMonth(){};
 let currentMonth;
 let summerMonth;
 let mylibraryFunction;
+
 //STEP 2
 2.2;
 'hello';
 false;
 null;
 //STEP3
+
 //COMPLEX EXPRESSION
 let comExp1 =3+(98*3)+33;
 let comExp2 =55+(43%2)+1;
 //VARIABLE EXPRESSION
 let varEx1=33;
 let varExp2=11.33;
+
 //STEP4
 let firstName;
 let lastName;
@@ -27,3 +30,19 @@ let intZipCode;
 let intYourAge;
 let referralSource;
 let boolMayWeContactYou;
+
+//step 5
+//first way
+let strFirstName1="rakhi" ;
+let strLasttName2="singh";
+let intYourAge1=33;
+//second way
+let strFirstNamee1="rakhi",strLasttNamee2="singh",
+intYourAgee1=33;
+ //third way
+ let strFirstName3="rakhi" ;
+let strLasttName3="singh";
+let intYourAge3=33;;
+ strFirstName3="rakhi" ;
+ strLasttName3="singh";
+ intYourAge3=33;;
